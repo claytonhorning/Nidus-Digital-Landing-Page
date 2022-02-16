443d44de5ef3e153ab7d5ebf55572b23da4c5cb9
Clayton Horning <clayton.horning@colorado.edu>
2022-02-16 13:42:14 -0700

    Added Content and More Sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    extend: {},
-  },
+    extend: {
+        keyframes: {
+            'fade-in-down': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateY(-10px)'
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateY(0)'
+                },
+            }
+        },
+        animation: {
+            'fade-in-down': 'fade-in-down 0.5s ease-out'
+        }
+    },
+},
   plugins: [
     function ({ addComponents }) {
       addComponents({
@@ -13,6 +29,7 @@ module.exports = {
           },
         }
       })
-    }
+    },
+    require('@tailwindcss/forms'),
   ]
 };
